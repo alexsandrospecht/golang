1bccf529f96c917a956bb608a22d5ba552ca8293
Alexsandro Specht <alexsandrospecht@gmail.com>
2017-09-24 22:17:31 -0300

    time server

--- high-order-functions/cronometro.go
+++ high-order-functions/cronometro.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	Cronometrar(GerarFibonnaci(8))
+	Cronometrar(GerarFibonnaci(48))
+	Cronometrar(GerarFibonnaci(88))
+}
+
+func GerarFibonnaci(n int) func() {
+	return func() {
+		a, b := 0, 1
+
+		fib := func() int {
+			a, b = b, a+b
+
+			return a
+		}
+
+		for i := 0; i < n; i++ {
+			fmt.Printf("%d ", fib())
+		}
+	}
+}
+
+func Cronometrar(funcao func()) {
+	inicio := time.Now()
+
+	funcao()
+
+	fmt.Printf("\nTempo de execução: %s\n\n", time.Since(inicio))
 }
